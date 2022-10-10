a95a9a44960afea4bd8c63c2f38062be18951a81
Pushpak Gosavi (NCS) <pushpak.gosavi@in.ncs-i.com>
2022-10-10 16:05:58 +0530

    retrofit class added

--- app/src/main/java/com/cartoon/boruto/utils/Constants.kt
+++ app/src/main/java/com/cartoon/boruto/utils/Constants.kt
@@ -1,6 +1,8 @@
 package com.cartoon.boruto.utils
 
 object Constants {
+    const val BASE_URL = "http://10.0.2.2:8080"
+
     const val BORUTO_DATABASE= "boruto_database"
     const val DETAILS_ARGUMENT_KEY= "heroId"
     const val HERO_DATABASE_TABLE = "hero_table"
